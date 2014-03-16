076e7ee5811d47b4b44676fe277811021914993e
William Howard <whoward.tke@gmail.com>
2014-03-16 02:49:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MintApp::Application.config.secret_key_base = 'e5f01f9873fc6644ce0fdfb846c84b72a46b7084c238cfe4c161a81af9cd27c75865d783e7c52f5e49d54073f1cfe69fbeb29d3a7ccebbe55c61234069bfde10'
