ee8c0c6e3625f352423d8e202be08e89397332d3
William Howard <whoward.tke@gmail.com>
2014-03-23 02:45:15 -0400

    dynamic categories in navigation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@
 class Category < ActiveRecord::Base
   acts_as_tree
   has_many :transactions
+
+  def transactions_count
+    # TODO: should include child category transactions count?
+    transactions.count
+  end
 end
