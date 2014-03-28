b7d0bb8ead51f23d05beca07e78c6f1494ff05d5
William Howard <whoward.tke@gmail.com>
2014-03-28 09:02:52 -0400

    many fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,9 +6,15 @@ class DashboardController < ApplicationController
 
     totals = Transaction.where(date: period).group(:category_id).sum(:cents)
 
-    @spending = Category.where(id: totals.keys).map do |c|
-      [c, totals[c.id] || 0]
-    end.sort_by(&:last).reverse
+    @income = Category.income.self_and_descendants.map do |cat|
+      [cat, totals[cat.id]]
+    end.reject {|c,t| t == nil }.sort_by(&:last).reverse
+
+    @expenses = Category.expense.self_and_descendants.map do |cat|
+      [cat, totals[cat.id]]
+    end.reject {|c,t| t == nil }.sort_by(&:last).reverse
+
+    @net_income = @income.map(&:last).sum - @expenses.map(&:last).sum
   end
 
 end
