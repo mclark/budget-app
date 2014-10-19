fb2e2ea90fcf64d7b044257fe4baa000956a45fa
Will Howard <whoward.tke@gmail.com>
2014-10-19 01:22:39 -0400

    better plot for debt report

--- app/controllers/reports/monthly_debt_controller.rb
+++ app/controllers/reports/monthly_debt_controller.rb
@@ -16,13 +16,16 @@ module Reports
     def to_csv
       CSV.generate do |csv|
         csv << %w(month net)
-        months.each {|year, month, net| csv << ["#{year}-#{month}", net] }
+        months.each {|date, net| csv << [date, net] }
       end
     end
 
     def months
       MonthEnumerator.since_first_transaction.map do |year, month|
-        [year, month, MonthlyDebtReport.new(Time.local(year, month, 1, 0, 0, 0)).net_income / 100.0]
+        time = Time.local(year, month, 1,  0, 0, 0)
+        stamp = (time.utc.to_f * 1000).to_i
+
+        [stamp, MonthlyDebtReport.new(time).net_income / 100.0]
       end
     end
 
