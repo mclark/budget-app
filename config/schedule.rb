91225e310185cf9bc96386b6e9011d8323230a42
Will Howard <whoward.tke@gmail.com>
2014-12-05 18:13:00 -0500

    log cron output to a file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-job_type :rake, "cd :path && RAILS_ENV=:rails_env rake :task"
+job_type :rake, "cd :path && RAILS_ENV=:rails_env rake :task >> log/cron.log 2>&1"
 
 every 1.day, at: "6:00pm" do
   rake "import:recent"
