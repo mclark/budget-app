8ec14fcac849fac0b3d44d67163af06c3fc67f29
Will Howard <whoward.tke@gmail.com>
2014-12-14 13:48:06 -0500

    run import twice a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 job_type :rake, "cd :path && RAILS_ENV=:rails_env bundle exec rake :task >> log/cron.log 2>&1"
 job_type :xvfb_rake, "cd :path && XVFB=true RAILS_ENV=:rails_env bundle exec rake :task >> log/cron.log 2>&1"
 
-every 1.day, at: "7:00am" do
+every 1.day, at: ["7:00am", "7:00pm"] do
   xvfb_rake "import:recent"
 end
